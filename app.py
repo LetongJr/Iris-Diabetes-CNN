8622330e7bd736154a0fc8a4cb453d0f03e6a6d3
LetongJr <wfreelance068@gmail.com>
2025-12-17 19:13:55 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -115,7 +115,5 @@ def health():
     return jsonify({"status": "CNN iris diabetes detector OK"})
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 8080))
-    print(f"Starting Flask server on port {port}...")
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
